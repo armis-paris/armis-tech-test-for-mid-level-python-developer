23b3266e09f651f982335bf48525ef7c10cfe9d8
Yanis Mohamdi <yanis.mohamdi@gmail.com>
2023-03-17 18:12:20 +0100

    first step of the technical assignment

--- src/service/text_process_service.py
+++ src/service/text_process_service.py
@@ -1,22 +1,38 @@
+def _append_if_not_exist(char: str, current_list: list) -> list:
+    """
+    I created this function instead of using a set() object to order the list by the first
+    position in the original string.
+    """
+    if char not in current_list:
+        current_list.append(char)
+    return current_list
+
+
 class TextProcessService:
 
     def extract_information(self, text: str) -> dict:
-        number_of_words: int = len(text.split())
-        length_of_text: int = len(text)
+        try:
+            number_of_words: int = len(text.split())
+            length_of_text: int = len(text)
 
-        unique_alphabetic_characters = []
-        unique_numerical_characters = []
-        unique_other_characters = []
+            unique_alphabetic_characters = []
+            unique_numerical_characters = []
+            unique_other_characters = []
 
-        for character in text:
-            if character.isalpha():
-                unique_alphabetic_characters.append(character)
-            if character.isnumeric():
-                unique_numerical_characters.append(character)
-            unique_other_characters.append(character)
+            for character in text:
+                if character.isalpha():
+                    _append_if_not_exist(character, unique_alphabetic_characters)
+                elif character.isnumeric():
+                    _append_if_not_exist(character, unique_numerical_characters)
+                else:
+                    _append_if_not_exist(character, unique_other_characters)
 
-        return {'number_of_words': number_of_words,
+            return {
+                'number_of_words': number_of_words,
                 'length_of_text': length_of_text,
-                'unique_alphabhetic_characters': unique_alphabetic_characters,
+                'unique_alphabetic_characters': unique_alphabetic_characters,
                 'unique_numerical_characters': unique_numerical_characters,
-                'unique_other_characters': unique_other_characters}
+                'unique_other_characters': unique_other_characters
+            }
+        except Exception as e:
+            raise Exception(f"Error while processing text: {e}")
