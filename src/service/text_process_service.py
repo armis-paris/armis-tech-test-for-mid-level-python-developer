915301c2fbbb1cc7a99426ace7bc195ebf29082e
yamen <yamen225@gmail.com>
2023-03-19 09:50:40 +0100

    fix and refactor text process service

--- src/service/text_process_service.py
+++ src/service/text_process_service.py
@@ -1,22 +1,51 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TextExtractedInformation:
+    number_of_words: int
+    length_of_text: int
+    unique_alphabetic_characters: set
+    unique_numerical_characters: set
+    unique_other_characters: set
+
+
 class TextProcessService:
+    def __init__(self, text: str):
+        if not isinstance(text, str):
+            # That can be enhanced by using a library for type enforcement like pydantic
+            raise Exception("Text must be a string")
 
-    def extract_information(self, text: str) -> dict:
-        number_of_words: int = len(text.split())
-        length_of_text: int = len(text)
+        # choose to save the text as private without any setter to avoid to change it.
+        self.__text = text
 
-        unique_alphabetic_characters = []
-        unique_numerical_characters = []
-        unique_other_characters = []
+        # Choose to save the base information in the object so to avoid to recompute it.
+        self.__length_of_text = len(text)
+        self.__number_of_words = len(text.split())
 
-        for character in text:
-            if character.isalpha():
-                unique_alphabetic_characters.append(character)
-            if character.isnumeric():
-                unique_numerical_characters.append(character)
-            unique_other_characters.append(character)
+    """
+    Even thought this implementation and previous one have the same O(n) complexity,
+    I chose to use multiple functions to enhance readability, and adhere to the single responsibility principle.
+    That can be enhanced in case of complicated logic to different classes that we can inject.
+    """
 
-        return {'number_of_words': number_of_words,
-                'length_of_text': length_of_text,
-                'unique_alphabhetic_characters': unique_alphabetic_characters,
-                'unique_numerical_characters': unique_numerical_characters,
-                'unique_other_characters': unique_other_characters}
+    def __get_unique_alphabetic_characters(self) -> set:
+        return {char for char in self.__text if char.isalpha()}
+
+    def __get_unique_numerical_characters(self) -> set:
+        return {char for char in self.__text if char.isnumeric()}
+
+    def __get_unique_other_characters(self) -> set:
+        return {
+            char for char in self.__text if not char.isalpha() and not char.isnumeric()
+        }
+
+    def extract_information(self) -> TextExtractedInformation:
+
+        return TextExtractedInformation(
+            number_of_words=self.__number_of_words,
+            length_of_text=self.__length_of_text,
+            unique_alphabetic_characters=self.__get_unique_alphabetic_characters(),
+            unique_numerical_characters=self.__get_unique_numerical_characters(),
+            unique_other_characters=self.__get_unique_other_characters(),
+        )
--- test/service/text_process_service_test.py
+++ test/service/text_process_service_test.py
@@ -1,18 +1,117 @@
 import unittest
-
+from parameterized import parameterized
 from src.service.text_process_service import TextProcessService
 
 
 class TextProcessServiceTest(unittest.TestCase):
-
     def test_should_extract_information(self):
         # given
-        text = 'I started to listen Nirvana band in 2009!'
+        text = "I started to listen Nirvana band in 2009!"
 
-        text_process_service = TextProcessService()
+        text_process_service = TextProcessService(text)
 
         # when
-        text_information = text_process_service.extract_information(text)
+        text_information = text_process_service.extract_information()
 
         # then
         self.assertIsNotNone(text_information)
+
+    @parameterized.expand(
+        [
+            ("Integers", 123),
+            ("None", None),
+            ("Floats", 1.23),
+            ("Boolean", True),
+            ("List", [1, 2, 3]),
+            ("Tuple", (1, 2, 3)),
+            ("Dictionary", {"a": 1, "b": 2, "c": 3}),
+        ]
+    )
+    def test_should_raise_exception_on_wrong_types(self, name, test_input):
+        # given
+        text = None
+
+        # when
+        with self.assertRaises(Exception, msg="Text must be a string"):
+            text_process_service = TextProcessService(test_input)
+            text_process_service.extract_information()
+
+    @parameterized.expand(
+        [
+            (
+                "normal_string",
+                "I started to listen Nirvana band in 2009!",
+                {"N", "t", "I", "n", "o", "i", "l", "d", "b", "a", "v", "e", "r", "s"},
+            ),
+            ("empty string", "", set()),
+        ]
+    )
+    def test_should_get_the_correct_unique_alpha_characters(
+        self, name, test_input, expected
+    ):
+        print(type(test_input))
+        # given
+        text_process_service = TextProcessService(test_input)
+
+        # when
+        text_information = text_process_service.extract_information()
+
+        # then
+        self.assertEqual(text_information.unique_alphabetic_characters, expected)
+
+    @parameterized.expand(
+        [
+            (
+                "normal_string",
+                "I started to listen Nirvana band in 2009!",
+                {"2", "0", "9"},
+            ),  # 2009
+            (
+                "empty string",
+                "",
+                set(),
+            ),
+        ]
+    )
+    def test_should_get_the_correct_unique_numeric_characters(
+        self, name, test_input, expected
+    ):
+        # given
+        text_process_service = TextProcessService(test_input)
+
+        # when
+        text_information = text_process_service.extract_information()
+
+        # then
+        self.assertEqual(text_information.unique_numerical_characters, expected)
+
+    @parameterized.expand(
+        [
+            (
+                "normal_string",
+                "I started to listen Nirvana band in 2009!",
+                {" ", "!"},
+            ),
+            ("empty string", "", set()),
+        ]
+    )
+    def test_should_get_the_correct_count_for_unique_other_characters(
+        self, name, test_input, expected
+    ):
+        # given
+        text_process_service = TextProcessService(test_input)
+
+        # when
+        text_information = text_process_service.extract_information()
+        # then
+        self.assertEqual(text_information.unique_other_characters, expected)
+
+    def test_canot_access_private_values(self):
+        # given
+        text = "I started to listen Nirvana band in 2009!"
+
+        text_process_service = TextProcessService(text)
+
+        # when
+        with self.assertRaises(AttributeError):
+            text_process_service.__text
